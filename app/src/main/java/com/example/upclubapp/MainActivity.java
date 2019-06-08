0d05f9590e893015e39ba0de0fbdf29c66d6e252
Алексей Михеев <amiheev@zennex.ru>
2019-06-08 14:35:42 +0700

    Add bottom navigation

--- app/src/main/java/com/example/upclubapp/MainActivity.java
+++ app/src/main/java/com/example/upclubapp/MainActivity.java
@@ -4,11 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity
+        implements AHBottomNavigation.OnTabSelectedListener {
+
+    @BindView(R.id.ahBottomNavigation)
+    AHBottomNavigation ahBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        initBottomNavigation();
+    }
+
+    private void initBottomNavigation() {
+        AHBottomNavigationAdapter adapter =
+                new AHBottomNavigationAdapter(this, R.menu.menu_bottom_navigaton);
+        adapter.setupWithBottomNavigation(ahBottomNavigation);
+    }
+
+    @Override
+    public boolean onTabSelected(int position, boolean wasSelected) {
+        switch (position) {
+            case 0: {
+                // TODO: Market
+                break;
+            }
+            case 1: {
+                // TODO: Chats
+                break;
+            }
+            case 2: {
+                // TODO: Profile
+                break;
+            }
+        }
+        return true;
     }
 }
