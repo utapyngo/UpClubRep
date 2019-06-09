c675794d87851c1188514f9d883d4c4bd1cde1f4
utapyngo <ut@pyngo.tom.ru>
2019-06-09 12:23:56 +0700

    Add reviews number

--- server/volunteers/serializers.py
+++ server/volunteers/serializers.py
@@ -4,7 +4,13 @@ from volunteers.models import Volunteer
 
 
 class VolunteerSerializer(serializers.ModelSerializer):
+    reviews_number = serializers.SerializerMethodField()
+
     class Meta:
         model = Volunteer
-        fields = ('user', 'rating', 'skills')
+        fields = ('user', 'rating', 'skills', 'reviews_number')
         read_only_fields = ('rating',)
+
+    # noinspection PyMethodMayBeStatic
+    def get_reviews_number(self, volunteer):
+        return volunteer.reviews_received.count()
